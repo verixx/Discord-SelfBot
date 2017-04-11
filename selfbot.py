d1661da21d154d823fda0fadff758469a285dfca
IgneelDxD <dxd.dragon.dxd@gmail.com>
2017-04-11 16:58:05 +0200

    :fire: changing logging directorties(last time, i promise)

--- selfbot.py
+++ selfbot.py
@@ -19,7 +19,7 @@ log.setLevel(logging.INFO)
 fileFormatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', '%Y-%m-%d %H:%M:%S')
 consoleFormatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', '%H:%M:%S')
 
-selfFile = logging.FileHandler(filename='Logs/SelfBot' + datetime.datetime.now().strftime("%Y-%m-%d") + '.log', encoding='utf-8', mode='a')
+selfFile = logging.FileHandler(filename='Logs/SelfBot/SelfBot' + datetime.datetime.now().strftime("%Y-%m-%d") + '.log', encoding='utf-8', mode='a')
 selfFile.setFormatter(fileFormatter)
 log.addHandler(selfFile)
 
@@ -30,7 +30,7 @@ log.addHandler(selfConsole)
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
 
-discordFile = logging.FileHandler(filename='Logs/Discord' + datetime.datetime.now().strftime("%Y-%m-%d") + '.log', encoding='utf-8', mode='a')
+discordFile = logging.FileHandler(filename='Logs/Discord/Discord' + datetime.datetime.now().strftime("%Y-%m-%d") + '.log', encoding='utf-8', mode='a')
 discordFile.setFormatter(fileFormatter)
 logger.addHandler(discordFile)
 
